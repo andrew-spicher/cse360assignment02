0799d7aa9e3d14ecfbd561ba9a770f41e7f87046
Spicher <Spicher@localhost>
2020-10-05 17:14:17 -0700

    Initial setup of java file

---
+++ Assignment 1 360/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,29 @@
+//William Andrew Spicher
+//CSE 360 Assignment 02
+
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not nee ded - included for clarity
+  }
+  
+  public int getTotal () {
+    return 0;
+  }
+  
+  public void add (int value) {
+  }
+
+  public void subtract (int value) {
+  }
+
+  public String toString () {
+    return "";
+  }
+
+  public void clear() {
+  }
+}
