89c57c3bfeaba7ab145b065f59ffc07eb644d9fe
Spicher <Spicher@localhost>
2020-10-05 18:38:58 -0700

    -Add method now adds its parameter to the total -Subtract method now subtracts its parameter from the total -getTotal method returns the total -toString now returns the history of computations -clear method now clears the total and log variables

--- Assignment 1 360/src/cse360assignment02/AddingMachine.java
+++ Assignment 1 360/src/cse360assignment02/AddingMachine.java
@@ -1,26 +1,78 @@
+/* This class adds, subtracts, allows users to see the total of added numbers, and clear
+ * the memory of added numbers
+ */
+
 package cse360assignment02;
 
-public class AddingMachine {
+public class AddingMachine 
+{
   private int total;
+  private String log = "0";
   
-  public AddingMachine () {
+  /* Constructor for AddingMachine
+   * This method sets the value of total to 0
+   */
+  public AddingMachine () 
+  {
     total = 0;  // not needed - included for clarity
   }
   
-  public int getTotal () {
-    return 0;
+  /* Returns the variable total
+   * 
+   * @return total
+   */
+  public int getTotal () 
+  {
+    return total;
   }
   
-  public void add (int value) {
+  /* This method adds the given parameter to the total variable.
+   * The method also increments the variable log  
+   * 
+   * @param value 	an int to be added
+   */
+  public void add (int value) 
+  {
+	  //Add param to total
+	  total = total + value;
+	  
+	  //Concat to log
+	  log += " + " + value;
   }
 
-  public void subtract (int value) {
+  /* This method subtracts the given parameter from the variable total
+   * The method also increments the variable log
+   * 
+   * @param value 	an int to be subtracted
+   */
+  public void subtract (int value) 
+  {
+	  //Subtract param from total
+	  total = total - value;
+	  
+	  //Concat to log
+	  log += " - " + value;
   }
 
-  public String toString () {
-    return "";
+  /* This method returns the log
+   * 
+   * @return log 	the variable log or list of computations
+   */
+  public String toString () 
+  {
+    return log;
   }
 
-  public void clear() {
-  }
+  /* This method clears the log and total variables
+   */
+  public void clear() 
+  {
+	  //Clear the log
+	  log = "0";
+	  //Clear total
+	  total = 0;
+  } 
 }
+
+
+
