94eb7f2ca833993b00679819d16abc426d8306eb
Spicher <Spicher@localhost>
2020-10-05 17:33:16 -0700

    Created original, unedited version of the file

--- Assignment 1 360/src/cse360assignment02/AddingMachine.java
+++ Assignment 1 360/src/cse360assignment02/AddingMachine.java
@@ -1,13 +1,10 @@
-//William Andrew Spicher
-//CSE 360 Assignment 02
-
 package cse360assignment02;
 
 public class AddingMachine {
   private int total;
   
   public AddingMachine () {
-    total = 0;  // not nee ded - included for clarity
+    total = 0;  // not needed - included for clarity
   }
   
   public int getTotal () {
